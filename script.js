fdfc33ce26d6ea4f39f94aa90725bb7c371062e8
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2021-07-20 04:28:32 -0600

    commit for 07/20/21 from working file

--- script.js
+++ script.js
@@ -6,71 +6,104 @@ let val20 = (document.querySelector(".value25").onload = loadFunction());
 let rewards = (document.querySelector('.reward1').onload = rewardLoad())
 
 function loadFunction() {
-    document.querySelector(".value25").style.borderBottom = borderInfo;
-  }
+  document.querySelector(".value25").style.borderBottom = borderInfo;
+}
 
 function rewardLoad() {
-    document.querySelector('.reward1').style.display = "block";
+  document.querySelector('.reward1').style.display = "block";
 }
+
+function value25() {
+  document.querySelector('.value25').style.borderBottom = borderInfo;
+  document.querySelector('.value50').style.borderBottom = 'none';
+  document.querySelector('.value150').style.borderBottom = 'none';
+  document.querySelector('.value200').style.borderBottom = 'none';
+  document.querySelector('.value400').style.borderBottom = 'none';
+  document.querySelector('.reward1').style.display = "block";
+  document.querySelector('.reward2').style.display = "none";
+  document.querySelector('.reward3').style.display = "none";
+  document.querySelector('.reward4').style.display = "none";
+  document.querySelector('.reward5').style.display = "none";
+}
+function value50() {
+  document.querySelector('.value50').style.borderBottom = borderInfo;
+  document.querySelector('.value25').style.borderBottom = "none";
+  document.querySelector('.value150').style.borderBottom = "none";
+  document.querySelector('.value200').style.borderBottom = "none";
+  document.querySelector('.value400').style.borderBottom = "none";
+  document.querySelector('.reward2').style.display = "block";
+  document.querySelector('.reward1').style.display = "none";
+  document.querySelector('.reward5').style.display = "none";
+  document.querySelector('.reward3').style.display = "none";
+  document.querySelector('.reward4').style.display = "none";
+}
+function value150() {
+  document.querySelector('.value150').style.borderBottom = borderInfo;
+  document.querySelector('.value25').style.borderBottom = 'none';
+  document.querySelector('.value50').style.borderBottom = 'none';
+  document.querySelector('.value200').style.borderBottom = 'none';
+  document.querySelector('.value400').style.borderBottom = 'none';
+  document.querySelector('.reward3').style.display = "block";
+  document.querySelector('.reward1').style.display = "none";
+  document.querySelector('.reward2').style.display = "none";
+  document.querySelector('.reward4').style.display = "none";
+  document.querySelector('.reward5').style.display = "none";
+
+}
+function value200() {
+  document.querySelector('.value200').style.borderBottom = borderInfo;
+  document.querySelector('.value25').style.borderBottom = 'none';
+  document.querySelector('.value50').style.borderBottom = 'none';
+  document.querySelector('.value150').style.borderBottom = 'none';
+  document.querySelector('.value400').style.borderBottom = 'none';
+  document.querySelector('.reward4').style.display = "block";
+  document.querySelector('.reward1').style.display = "none";
+  document.querySelector('.reward2').style.display = "none";
+  document.querySelector('.reward3').style.display = "none";
+  document.querySelector('.reward5').style.display = "none";
+}
+function value400() {
+  document.querySelector('.value400').style.borderBottom = borderInfo;
+  document.querySelector('.value25').style.borderBottom = 'none';
+  document.querySelector('.value50').style.borderBottom = 'none';
+  document.querySelector('.value150').style.borderBottom = 'none';
+  document.querySelector('.value200').style.borderBottom = 'none';
+  document.querySelector('.reward5').style.display = "block";
+  document.querySelector('.reward1').style.display = "none";
+  document.querySelector('.reward2').style.display = "none";
+  document.querySelector('.reward3').style.display = "none";
+  document.querySelector('.reward4').style.display = "none";
+}
+
+function addText() {
+  document.querySelector('#text').placeholder = "";
+
+  let addBox = document.querySelector('.fieldset');
+  let newText = document.createElement("legend")
+  newText.classList.add('legend')
+  newText.innerHTML = "Enter Your Star Code";
   
-  function value25() {
-    document.querySelector('.value25').style.borderBottom = borderInfo;
-    document.querySelector('.value50').style.borderBottom = 'none';
-    document.querySelector('.value150').style.borderBottom = 'none';
-    document.querySelector('.value200').style.borderBottom = 'none';
-    document.querySelector('.value400').style.borderBottom = 'none';
-    document.querySelector('.reward1').style.display = "block";
-    document.querySelector('.reward2').style.display = "none";
-    document.querySelector('.reward3').style.display = "none";
-    document.querySelector('.reward4').style.display = "none";
-    document.querySelector('.reward5').style.display = "none";
+  let leg = document.querySelector('.legend');
+  if (typeof(leg) != 'undefined' && leg != null) {
+    console.log("already exists")
   }
-  function value50() {
-    document.querySelector('.value50').style.borderBottom = borderInfo;
-    document.querySelector('.value25').style.borderBottom = "none";
-    document.querySelector('.value150').style.borderBottom =  "none";
-    document.querySelector('.value200').style.borderBottom = "none";
-    document.querySelector('.value400').style.borderBottom =  "none";
-    document.querySelector('.reward2').style.display = "block";
-    document.querySelector('.reward1').style.display = "none";
-    document.querySelector('.reward5').style.display = "none";
-    document.querySelector('.reward3').style.display = "none";
-    document.querySelector('.reward4').style.display = "none";
+  else {
+    document.querySelector('.fieldset').appendChild(newText);
+    document.querySelector('.fieldset').style = "border-color: #008248";
+    newText.style = "color: #008248";
   }
-  function value150() {
-    document.querySelector('.value150').style.borderBottom = borderInfo;
-    document.querySelector('.value25').style.borderBottom = 'none';
-    document.querySelector('.value50').style.borderBottom = 'none';
-    document.querySelector('.value200').style.borderBottom = 'none';
-    document.querySelector('.value400').style.borderBottom = 'none';
-    document.querySelector('.reward3').style.display = "block";
-    document.querySelector('.reward1').style.display = "none";
-    document.querySelector('.reward2').style.display = "none";
-    document.querySelector('.reward4').style.display = "none";
-    document.querySelector('.reward5').style.display = "none";
-    
+}
+
+function inputInfo() {
+  let starCode = document.getElementById("text").value
+  console.log(starCode);
+
+  if (typeof starCode === "undefined" || starCode === null || starCode === "") {
+    document.querySelector('.fieldset').style = "border-color: red";
+    document.querySelector('.legend').style.color = "red"
+  } else {
+    document.querySelector('.fieldset').style = "border-color: #008248";
+    document.querySelector('.legend').style.color = "#008248";
+    alert("Thank your for your Star Code");
   }
-  function value200() {
-    document.querySelector('.value200').style.borderBottom = borderInfo;
-    document.querySelector('.value25').style.borderBottom = 'none';
-    document.querySelector('.value50').style.borderBottom = 'none';
-    document.querySelector('.value150').style.borderBottom = 'none';
-    document.querySelector('.value400').style.borderBottom = 'none';
-    document.querySelector('.reward4').style.display = "block";
-    document.querySelector('.reward1').style.display = "none";
-    document.querySelector('.reward2').style.display = "none";
-    document.querySelector('.reward3').style.display = "none";
-    document.querySelector('.reward5').style.display = "none";
-  }
-  function value400() {
-    document.querySelector('.value400').style.borderBottom = borderInfo;
-    document.querySelector('.value25').style.borderBottom = 'none';
-    document.querySelector('.value50').style.borderBottom = 'none';
-    document.querySelector('.value150').style.borderBottom = 'none';
-    document.querySelector('.value200').style.borderBottom = 'none';
-    document.querySelector('.reward5').style.display = "block";
-    document.querySelector('.reward1').style.display = "none";
-    document.querySelector('.reward2').style.display = "none";
-    document.querySelector('.reward3').style.display = "none";
-    document.querySelector('.reward4').style.display = "none";
-  }
+}
